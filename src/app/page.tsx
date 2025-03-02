f58d09e70b8fcd440a764a8d4aa6f5c697562f97
Kushagra <skushagra.sharma@gmail.com>
2025-03-02 17:39:25 +0530

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
 
 export default function Home() {
   const router = useRouter();
   
-  useEffect(() => {
     router.push('/merchant');
-  }, []);
 
   return null;
 }
